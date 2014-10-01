720ff0cf397bf6fbbc9d3b3d1ec1ae682d309b9b
Chris Lynch <chrislynch42@yahoo.com>
2014-09-30 22:17:54 -0400

    Added database_config_path

--- lib/rails/generators/lesswork/migration_generator.rb
+++ lib/rails/generators/lesswork/migration_generator.rb
@@ -3,12 +3,23 @@ require "active_record/migration"
 require "rails"
 require "rails/generators"
 require "rails/generators/active_record/migration"
+require "active_support"
 
 module LessWork
 
   class MigrationGenerator < Rails::Generators::Base
     include Rails::Generators::Migration
     include ActiveRecord::Generators::Migration
+
+    class << self
+      attr_accessor :database_config_path
+    end
+    self.database_config_path = Dir.pwd + '/config/database.yml'
+
+
+
+
+
 
   end
 end
--- test/rails/generators/lesswork/migration_generator_test.rb
+++ test/rails/generators/lesswork/migration_generator_test.rb
@@ -5,17 +5,19 @@ require_relative '../../../../lib/rails/generators/lesswork/migration_generator'
 class MigrationGeneratorTest < Rails::Generators::TestCase
   tests LessWork::MigrationGenerator
 
-  def test_setup
-    assert_equal(true, true)
-    puts destination_root
+  def test_assertion
+    assert(true, "Assertions did not work")
   end
-
 
   def test_run_generator
     content = run_generator ["--help"]
-    puts content
     assert_match(/Runtime options:/, content)
     assert_match(/Description:/, content)
+  end
+
+  def test_database_path
+    assert(LessWork::MigrationGenerator.respond_to? :database_config_path, 'LessWork::MigrationGenerator did not have database_config_path class method')
+    assert(LessWork::MigrationGenerator.database_config_path.end_with?( 'test/config/database.yml'),'Database config path did not contain test/config/database.yml')
   end
 
 end
